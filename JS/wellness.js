c418b37e242194a0d36d2c6e5954490164ebf69a
Vaibhav Raj <vaibhavraj524@gmail.com>
2021-06-12 10:47:32 +0530

    changes in add to cart page

--- JS/wellness.js
+++ JS/wellness.js
@@ -1,26 +1,21 @@
-let addToCartButton = document.getElementsByClassName("addToCart");
+let addCartBtn = document.getElementsByClassName("addCart");
 
-let arr = JSON.parse(localStorage.getItem("Products")) || [];
+// console.log(addCartBtn[0].parentElement);
 
-for (var i = 0; i < addToCartButton.length; i++) {
-  var anchor = addToCartButton[i];
+for (var i = 0; i < addCartBtn.length; i++) {
+  var anchor = addCartBtn[i];
   anchor.onclick = function () {
+    let arr = JSON.parse(localStorage.getItem("products")) || [];
+
     const object = {
       image: this.parentElement.children[0].src,
       discount: this.parentElement.children[1].innerHTML,
       name: this.parentElement.children[2].innerHTML,
       Mfr: this.parentElement.children[3].innerHTML,
-
       originalPrice: this.parentElement.children[4].children[0].innerHTML,
       discountPrice: this.parentElement.children[4].children[1].innerHTML,
     };
 
-    // let arr = localStorage.getItem("Products");
-    // if (arr == null) {
-    //   arr = [];
-    // } else {
-    //   arr = JSON.parse(arr);
-    // }
     arr.push(object);
     localStorage.setItem("products", JSON.stringify(arr));
   };
